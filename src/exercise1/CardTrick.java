b475f7e4059c9b8eb65293132951d421f4ebac27
Karam Abuzaid <abuzaid@sheridancollege.ca>
2023-05-26 10:26:20 -0400

    added card code to CardTrick.java

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -13,34 +13,44 @@ public class CardTrick {
     public static void main(String[] args) {
         
         Card[] hand = new Card[7];
+        Random random = new Random();
 
         for (int i = 0; i < hand.length; i++) {
             Card card = new Card();
-            //card.setValue(insert call to random number generator here)
-            // 
-            //card.setSuit(Card.SUITS[insert call to random number between 0-3 here])
-            // Hint: You can use Random -> random.nextInt(n) to get a random number between 0 and n-1 (inclusive)
-            //       Don't worry about duplicates at this point
+            card.setValue(random.nextInt(13) + 1);
+            card.setSuit(Card.SUITS[random.nextInt(4)]);
+            hand[i] = card;
         }
 
-        // insert code to ask the user for Card value and suit, create their card
-        // and search the hand here. 
-        // Hint: You can ask for values 1 to 10, and then
-        //       11 for jack, 12 for queen, etc. (remember arrays are 0-based though)
-        //       1 for Hearts, 2 for Diamonds, etc. (remember arrays are 0-based though)
-        // 
-        // Then loop through the cards in the array to see if there's a match.
-        
-        // If the guess is successful, invoke the printInfo() method below.
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the value of your card (1-10, 11 for Jack, 12 for Queen, 13 for King): ");
+        int value = scanner.nextInt();
+        System.out.print("Enter the suit of your card (0 for Hearts, 1 for Diamonds, 2 for Clubs, 3 for Spades): ");
+        int suit = scanner.nextInt();
+
+        Card userCard = new Card();
+        userCard.setValue(value);
+        userCard.setSuit(Card.SUITS[suit]);
+
+        boolean foundMatch = false;
+        for (Card card : hand) {
+            if (card.getValue() == userCard.getValue() && card.getSuit() == userCard.getSuit()) {
+                foundMatch = true;
+                break;
+            }
+        }
+
+        if (foundMatch) {
+            printInfo();
+        }
     }
 
     /**
-     * A simple method to print out personal information. Follow the instructions to 
-     * replace this information with your own.
-     * @author Paul Bonenfant Jan 2022
+     * A simple method to print out personal information.
+     * Replace this information with your own.
      */
     private static void printInfo() {
+
     
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
